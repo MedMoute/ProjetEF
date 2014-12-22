5ea1276766f1935b0971ebc287762dc367a4f34c
MedMoute <mehdi.ennaime@gmail.com>
2014-12-22 19:38:50 +0100

    Encapsulation rétablie

--- include/maillage.h
+++ include/maillage.h
@@ -11,7 +11,7 @@ using namespace std;
 class Maillage {
   
   // membres
- public :
+ private :
   int n_nodes ;     // nb de noeuds
   int n_elems ;     // nb d'elements 
   int n_triangles ; // nb de triangles parmi les elements
@@ -20,9 +20,9 @@ class Maillage {
   int* nodes_ref ;   // ref. des noeuds
   int* elems_type ;  // type de chaque element et nb de sommets
   int* elems_ref ;   // ref. des elements
+  int* elems_sommets ; // ref. des sommets de chaque element
   int* triangles_ref ; // C EST QUOI
   int* triangles_sommets ; // donne acces au num global d un des trois sommets de chaque triangle
-  int* elems_sommets ; // ref. des sommets de chaque element
   int* n_partition; // tmp[5] = 5eme tag de l'element -> nbre de partition de l'élément X
   int* partition_ref; //tmp[6] = 6eme tag de l'element ->partition "principale" de l'élément
 
@@ -37,8 +37,41 @@ class Maillage {
  public :
   // constructeur a partir d'un fichier
   Maillage(ifstream&);
-
   ~Maillage() ;
+  // Méthodes set pour l'encapsulation de la classe
+  void Set_n_nodes (int _n_nodes);
+  void Set_n_elems (int _n_elems);
+  void Set_n_triangles (int _n_triangles);
+  void Set_nb_partitions (int _n_partitions);
+
+  void Set_nodes_coords (double* _nodes_coords);
+  void Set_nodes_ref (int* _nodes_ref);
+  void Set_elems_type (int* _elems_type);
+  void Set_elems_ref (int* _elems_ref);
+  void Set_elems_sommets (int* _elems_sommets);
+  void Set_triangles_ref (int* _triangles_ref);
+  void Set_triangles_sommets (int* _triangles_sommets);
+  void Set_n_partition (int* _n_partition);
+  void Set_partition_ref (int* _partition_ref);
+
+  //Méthodes get pour l'encapsulation 
+  int Get_n_nodes ();
+  int Get_n_elems ();
+  int Get_n_triangles();
+  int Get_nb_partitions();
+
+  double* Get_nodes_coords();
+  int* Get_nodes_ref();
+  int* Get_elems_type();
+  int* Get_elems_ref();
+  int* Get_elems_sommets();
+  int* Get_triangles_ref();
+  int* Get_triangles_sommets();
+  int* Get_n_partition();
+  int* Get_partition_ref();
+
+  //Destructeur
+
 };
 #endif // MAILLAGE_H
 
--- include/probleme.h
+++ include/probleme.h
@@ -12,7 +12,7 @@ typedef Eigen::Matrix<double, Eigen::Dynamic, 1> VectorXd ;
 class Probleme {
 
     // membres
-public :
+private :
     Maillage* maillage;
     VectorXd* uexa ;
     VectorXd* g ;
@@ -23,14 +23,13 @@ public :
    * le noeud est sur l'interface */
     int* partition_noeud;
 
-    /*voisins_interfaces est un vecteur de vecteurs ; le ième vecteur contient l'ensemble
+    /*voisins_interface est un vecteur de vecteurs ; le ième vecteur contient l'ensemble
    *  des points de la partition i voisins de l'interface */
     vector<vector<int> > voisins_interface;
 
     /*voisins_partition est un vecteur de vecteurs ; le ieme vecteur contient l'ensemble
     * des points de l'interface voisins de la partition i */
     vector<vector<int> > voisins_partition;
-
 
     //int *tab_local_global;
     Eigen::SparseMatrix<double> *p_K ; // matrice K
@@ -50,13 +49,42 @@ public :
     void mat_K_elem(double* , double* , double );
     void assemblage_pKelem(int , double* , int );
 
-    /*Calcule, par parcours des noeuds de l'interface, voisins_interface et voisins_partitions */
+    /*Calcule, par parcours des noeuds de l'interface, voisins_interface et voisins_partition */
     void calcul_voisins(vector<vector<int> >, vector<vector<int> >);
 
+    //Méthodes de Set pour l'encapsulation
+    void Set_maillage (Maillage* _maillage);
+    void Set_uexa (VectorXd* _uexa);
+    void Set_g (VectorXd* _g);
+    void Set_u (VectorXd* _u);
+    void Set_felim (VectorXd* _felim);
+
+    void Set_partition_noeud (int* _partition_noeud);
+    void Set_voisins_interface (vector<vector<int> > _voisins_interface);
+    void Set_voisins_partition (vector<vector<int> > _voisins_partition);
+
+    void Set_p_K (Eigen::SparseMatrix<double>* _p_K);
+    void Set_p_Kelim (Eigen::SparseMatrix<double>* _p_Kelim);
+    void Set_diag (Eigen::DiagonalMatrix<double,Eigen::Dynamic>* _diag);
+
+    //Méthodes "Get" pour l'encapsulation
+
+    Maillage* Get_maillage ();
+    VectorXd* Get_uexa ();
+    VectorXd* Get_g ();
+    VectorXd* Get_u ();
+    VectorXd* Get_felim ();
+
+    int* Get_partition_noeud ();
+    vector<vector<int> > Get_voisins_partition ();
+    vector<vector<int> > Get_voisins_interface ();
+
+    Eigen::SparseMatrix<double>* Get_p_K ();
+    Eigen::SparseMatrix<double>* Get_p_Kelim ();
+    Eigen::DiagonalMatrix<double,Eigen::Dynamic>* Get_diag ();
+
+    //Destructeur
     ~Probleme();
-
-
-
 
     void pelim(int) ;
 
