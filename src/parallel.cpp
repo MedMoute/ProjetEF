9ca3999fe2cf6a03a3afcf42a579a87b373e426c
pifouuu <pierre00fournier@gmail.com>
2014-12-24 01:47:18 +0100

    toujours les recv qui posent probleme, mais les données fournies lors de l'appel semblent etre bonnes d'apres gdb

--- src/parallel.cpp
+++ src/parallel.cpp
@@ -16,35 +16,39 @@ void communication(VectorXd u, vector<vector<int> > voisins_partition, vector<ve
     {
         cout << "Task : "<<rang<< " Est entrée dans la fonction de communication de l'interface" << endl;
         vector<vector<double> > valeurs_a_envoyer;
-        vector<vector<double> > valeurs_a_recevoir;
         valeurs_a_envoyer.resize(nb_procs-1);
+        vector<vector<double> > valeurs_a_recevoir;
         valeurs_a_recevoir.resize(nb_procs-1);
         for (int i=1;i<nb_procs;i++)
         {
-            for (unsigned int j=0;j<voisins_partition[i].size();j++)
+            valeurs_a_envoyer[i-1].resize(voisins_partition[i-1].size());
+            valeurs_a_recevoir[i-1].resize(voisins_interface[i-1].size());
+            for (unsigned int j=0;j<voisins_partition[i-1].size();j++)
             {
                 valeurs_a_envoyer[i-1].push_back(u.coeffRef(voisins_partition[i-1][j]-1,0));
             }            
             MPI_Send(&valeurs_a_envoyer[i-1][0],valeurs_a_envoyer[i-1].size(),MPI_DOUBLE,i,0,MPI_COMM_WORLD);
-            MPI_Recv(&valeurs_a_recevoir[i-1][0],voisins_interface[i-1].size(),MPI_DOUBLE,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+            /*MPI_Recv(&valeurs_a_recevoir[i-1][0],voisins_interface[i-1].size(),MPI_DOUBLE,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
             for (int k=0;k<valeurs_a_recevoir[i-1].size();k++)
             {
                 u.coeffRef(voisins_interface[i-1][k],0)=valeurs_a_recevoir[i-1][k];
-            }
+            }*/
         }
     }
     else
     {
         cout << "Task : "<<rang<< " Est entrée dans la fonction de communication de la non-interface" << endl;
-        affiche_vector(voisins_interface);
         vector<double> valeurs_a_envoyer;
+        valeurs_a_envoyer.resize(voisins_interface[rang-1].size());
         vector<double> valeurs_a_recevoir;
+        valeurs_a_recevoir.resize(voisins_partition[rang-1].size());
         for (unsigned int i=0;i<voisins_interface[rang-1].size();i++)
         {
             valeurs_a_envoyer.push_back(u.coeffRef(voisins_interface[rang-1][i]-1,0));
         }
         MPI_Recv(&valeurs_a_recevoir[0],voisins_partition[rang-1].size(),MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-        MPI_Send(&valeurs_a_envoyer[0],valeurs_a_envoyer.size(),MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+        /*MPI_Send(&valeurs_a_envoyer[0],valeurs_a_envoyer.size(),MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+        */
         for (int i=0;i<valeurs_a_recevoir.size();i++)
         {
             u.coeffRef(voisins_partition[rang-1][i],0)=valeurs_a_recevoir[i];
